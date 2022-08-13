d33e1b2ab24b6c0580db3f0a7d25aed525e31ed2
Shriram Sekar <shriramsekar11@gmail.com>
2022-08-13 17:25:25 +0400

    meta: reset project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod renderer;
 fn main() {
-    let mut screen = renderer::renderer::Renderer::new(10);
-
-    screen
-        .event_loop
-        .run(move |event, _, control_flow| screen.window.request_redraw());
+    println!("cRustyChip!");
 }
